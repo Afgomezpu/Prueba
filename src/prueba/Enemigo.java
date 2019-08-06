1d8f94ccd93a51ce1d597f0c3ad0681c0e7f3cd5
Afgomezpu <41136520+Afgomezpu@users.noreply.github.com>
2019-08-05 20:13:21 -0500

    message

---
+++ src/prueba/Enemigo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author 57318
+ */
+public class Enemigo {
+     int peligrosidad=0;
+   int vida=0;
+}
---
+++ src/prueba/Isla.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author 57318
+ */
+public class Isla {
+    String nombre="";
+    LinkedList<Esclavo> Esclavos;
+}
